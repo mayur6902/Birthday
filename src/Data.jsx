8f5cd6ff86adbd5c11cfb977971dd1165d230dbb
mayur6902 <mayurkale38@gmail.com>
2023-10-26 01:23:03 +0530

    first commit

---
+++ src/Data.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Data =[
+{title:'Mayur',img:'https://media.licdn.com/dms/image/C5603AQF7pqX5REWtKQ/profile-displayphoto-shrink_200_200/0/1631960736450?e=1703721600&v=beta&t=l1U5dImWtTdBZmFkrnqr7Ob-vSHUM4m2IqkLQPtgaSU' , Age:22},
+
+{title:'Shubhankar',img:'https://media.licdn.com/dms/image/D5603AQFuIVPo2mIweQ/profile-displayphoto-shrink_200_200/0/1671687065510?e=1703721600&v=beta&t=lC5kRSrIoLk21auL65a41vu0R60wwSe17YoPq_aNq9g',Age:21},
+
+{title:'Amruta',img:'https://media.licdn.com/dms/image/C4E03AQF3ZjNRH_kg8Q/profile-displayphoto-shrink_200_200/0/1646654937452?e=1703721600&v=beta&t=qGRGOE4JvRq8VEAKFitF8vH8r_V_t0oOKonhrWMgz74', Age:23},
+
+{title:'Kunal',img:'https://media.licdn.com/dms/image/D5635AQE7zxtY3Zgmgw/profile-framedphoto-shrink_200_200/0/1696788127130?e=1698865200&v=beta&t=lWkgo6vD2HuvxRHG6K5e5kVVXugHj5xCgeiJzvO3Qps' , Age:24},
+
+{title:'Umesh',img:'https://media.licdn.com/dms/image/D4D35AQE_nwKKXqyZDw/profile-framedphoto-shrink_200_200/0/1693502935587?e=1698865200&v=beta&t=HkwEYxIVmOSaad-cMYRB6Gy7_RiVozc4EepNWBjRc7g' , Age:25},
+
+{title:'Tanishka',img:'https://media.licdn.com/dms/image/D5603AQH-pgn_mP_sOg/profile-displayphoto-shrink_200_200/0/1695864741189?e=1703721600&v=beta&t=9aWBYt8qeOfmNY5jJRPkiroZZhjqBQyK1jdbeK7FoaQ' , Age:27},
+
+
+];
